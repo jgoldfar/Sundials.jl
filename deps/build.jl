bc8df5b8869adf0460a67d103be7819d1db2dc0c
Jonathan Goldfarb <jgoldfar@gmail.com>
2017-11-01 16:25:24 -0400

    Update build script for v3.0.0

--- deps/build.jl
+++ deps/build.jl
@@ -15,25 +15,30 @@ kinsol = library_dependency("libsundials_kinsol")
 nvecserial = library_dependency("libsundials_nvecserial")
 sundialslibs = enable_sensitivities ? [cvodes, idas, kinsol, nvecserial] : [cvode, ida, kinsol, nvecserial]
 
-sundialsver = "sundials-2.5.0"
+sundialsver = "sundials-3.0.0"
 provides(Sources,
-    URI("http://ftp.mcs.anl.gov/pub/petsc/externalpackages/$sundialsver.tar.gz"),
-    SHA="9935760931fa6539edd0741acbcf4986770426fd5ea40e50ad4ebed0fc77b0d3",
+    URI("https://computation.llnl.gov/projects/sundials/download/$sundialsver.tar.gz"),
+    SHA="28b8e07eecfdef66e2c0d0ea0cb1b91af6e4e94d71008abfe80c27bf39f63fde",
     sundialslibs)
 
-provides(Binaries, URI("https://bintray.com/artifact/download/tkelman/generic/$sundialsver.7z"),
-    SHA="81b168ee98a680e9db80a5de88a5fcc5c05ce81eeb0b211f8352996972a026e6",
-    sundialslibs, unpacked_dir="usr$(Sys.WORD_SIZE)/bin", os = :Windows)
+# provides(Binaries, URI("https://bintray.com/artifact/download/tkelman/generic/$sundialsver.7z"),
+#     SHA="81b168ee98a680e9db80a5de88a5fcc5c05ce81eeb0b211f8352996972a026e6",
+#     sundialslibs, unpacked_dir="usr$(Sys.WORD_SIZE)/bin", os = :Windows)
 
 prefix = joinpath(BinDeps.depsdir(sundialslibs[1]),"usr")
+examplePrefix = joinpath(BinDeps.depsdir(sundialslibs[1]),"usr", "examples")
 srcdir = joinpath(BinDeps.depsdir(sundialslibs[1]),"src",sundialsver)
+builddir = joinpath(BinDeps.depsdir(sundialslibs[1]), "builddir")
 
 provides(SimpleBuild,
     (@build_steps begin
         GetSources(sundialslibs[1])
+        CreateDirectory(builddir)
+        CreateDirectory(prefix)
         @build_steps begin
-            ChangeDirectory(srcdir)
-            `./configure --prefix=$prefix --enable-shared`
+            ChangeDirectory(builddir)
+            `cmake -DCMAKE_INSTALL_PREFIX=$(prefix) -DEXAMPLES_INSTALL_PATH=$(examplePrefix) $(srcdir)`
+            `make`
             `make install`
         end
     end), sundialslibs)
